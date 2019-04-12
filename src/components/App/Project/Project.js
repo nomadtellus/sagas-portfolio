2826f57f66432e6aa53daa5bf2ffb4423a1485ef
Rowan Smith <theintheredwoods@gmail.com>
2019-04-12 14:51:02 -0500

    project display first draft done

---
+++ src/components/App/Project/Project.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+class Project extends Component {
+
+    componentDidMount() {
+    this.props.dispatch( { type: 'SET_PROJECTS' } );
+
+}
+
+
+// use component did mount to dispatch an action to request the plantList from the API
+    
+
+    render() {
+        return (
+            <div>
+                {this.props.reduxState.projects.map(project => (
+                    <div key={project.id}>
+                    <h2>{project.name}</h2>
+                    <img src={project.thumbnail}></img>
+                    <p>{project.description}</p>
+                    <p>{project.website}</p>
+                    <p>{project.date_completed}</p>
+                    <p>{project.tag}</p>
+                    </div>
+                ) 
+            </div>      
+        ;
+    }
+}
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
+export default connect(mapStateToProps)(PlantList);
